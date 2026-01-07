7b41d643add556849540cc5c25c11cb8bc8a184b
Andrea030105 <tummoloandrea05@gmail.com>
2026-01-07 17:27:17 +0100

    bonus-done

---
+++ src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            img:'/public/jumbo.png'
+        }
+    },
+}
+</script>
+<template>
+    <div>
+        <img :src="img" > 
+    </div>
+</template>
+<style scoped>
+    img{
+        width: 100%;
+    }
+</style>
