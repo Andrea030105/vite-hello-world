d2117825c4ed23afdecba5c7d6ba719609eaa313
Andrea030105 <tummoloandrea05@gmail.com>
2026-01-07 17:08:22 +0100

    ex-done

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data(){
+        return{
+            title:'La mia prima app con Vite!!',
+        }
+    }
+}
+</script>
+<template>
+    <div class="main">
+       {{ title }} 
+    </div>
+</template>
+<style scoped>
+    .main{
+        color: red;
+        font-size: 2rem;
+        font-weight: bold;
+    }
+</style>
